618506b582a02555ee2aa9cf69f4d72c2b77f83d
chaozhe2008 <997852533@qq.com>
2023-12-09 21:34:47 -0800

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 require("dotenv").config();
-const helper = require("./server/apis/helper");
 const postsRouter = require("./server/apis/posts");
 const usersRouter = require("./server/apis/users");
 const app = express();
